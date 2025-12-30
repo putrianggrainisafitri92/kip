139d1cc92f59fb78a22e1de22b13bbc3c49455b9
putrianggrainisafitri92 <putrianggrainisafitri92@gmail.com>
2025-12-30 13:30:37 +0700

    1

--- statistik/get_sk.php
+++ statistik/get_sk.php
@@ -10,7 +10,7 @@ $res = $koneksi->query("SELECT nama_sk, file_path
 
 $list = [];
 while($row = $res->fetch_assoc()){
-    $file_url = "uploads/sk/" . basename($row['file_path']);
+    $file_url = 'https://sisteminformasikipk.online/uploads/sk/' . $row['file_path'];
     $list[] = ['nama_sk'=>$row['nama_sk'], 'file_url'=>$file_url];
 }
 
